3239c86eac2da7aec3fb20385b9b53023d9f66ef
iStormathy <stormageddon473@yahoo.com>
2026-04-02 15:19:54 -0400

    Added comments explaining what the program and lines 10, 17, and 26 do.

--- Days_In_A_Month_Project/Days_In_A_Month_Project.cpp
+++ Days_In_A_Month_Project/Days_In_A_Month_Project.cpp
@@ -1,3 +1,5 @@
+// This program asks for a month and a year and
+// displays how many days were in that month.
 #include <iostream>
 using namespace std;
 int main()
@@ -5,14 +7,14 @@ int main()
 	int days, choice, year;
 	cout << "Enter a number corresponding to a month: ";
 	cin >> choice;
-	if (choice < 1 || choice > 12)
+	if (choice < 1 || choice > 12) // Limits the choice to a range of 1-12
 		cout << "Invalide input, try again.";
 	else
 	{
 		cout << "Enter a year: ";
 		cin >> year;
 
-		if (choice == 2 && (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+		if (choice == 2 && (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) // Equation to account for leap years
 			days = 29;
 		else if
 			(choice == 2)
@@ -21,7 +23,7 @@ int main()
 			(choice == 4 || choice == 6 || choice == 9 || choice == 11)
 			days = 30;
 		else
-			days = 31;
+			days = 31; // Default number, since there are more months with 31 days than not.
 		cout << "That month had, or will have, " << days << " days in it." << endl;
 	}
 	return 0;
